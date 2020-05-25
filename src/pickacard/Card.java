0588dc02e7c99a8190457184a043d6f042cf3423
ProfPaulB <paulbonenfant@hotmail.com>
2020-05-25 10:34:56 -0400

    First Commit

---
+++ src/pickacard/Card.java
@@ -0,0 +1,49 @@
+package pickacard;
+
+/**
+ * A class that models playing card Objects. Cards have 
+ * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers.
+ * This code is to be used in Exercise 1. When you create your own branch,
+ * add your name as an author.
+ * @author dancye
+ * @author Paul Bonenfant
+ */
+public class Card {
+
+   private String suit; //clubs, spades, diamonds, hearts
+   private int value;//1-13
+
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+   
+   
+    
+}
---
+++ src/pickacard/CardTrick.java
@@ -0,0 +1,27 @@
+package pickacard;
+
+/**
+ * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
+ * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
+ *
+ * @author dancye
+ * @author Paul Bonenfant
+ */
+public class CardTrick {
+
+    public static void main(String[] args) {
+        
+        Card[] magicHand = new Card[7];
+
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            //c.setValue(insert call to random number generator here)
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        }
+
+        //insert code to ask the user for Card value and suit, create their card
+        // and search magicHand here
+        //Then report the result here
+    }
+
+}
