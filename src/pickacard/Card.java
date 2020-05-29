334cd5bc16318d8e75d224fb8483df098005af30
anmolsekhon590 <54895453+anmolsekhon590@users.noreply.github.com>
2020-05-29 16:40:26 -0400

    - Added name and Date

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -1,14 +1,9 @@
 package pickacard;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in Exercise 1. When you create your own branch,
- * add your name as an author.
- * @author dancye
- * @author Paul Bonenfant
+ * @author anmol sekhon
+ * 
+ * May 29, 2020
  */
 public class Card {
 
--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,11 +1,9 @@
 package pickacard;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
- *
- * @author dancye
- * @author Paul Bonenfant
+ * @author anmol sekhon
+ * 
+ * May 29, 2020
  */
 public class CardTrick {
 
