4d482e9b598504499a73e86cb4a7ef007d0f1425
tommychoi914 <65785126+tommychoi914@users.noreply.github.com>
2020-05-30 13:59:13 -0400

    Update CardTrick.java

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -12,6 +12,10 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
+        
+        Card luckyCard = new Card();
+        luckyCard.setValue(9);
+        luckyCard.setSuit("Diamonds");
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
