d5f3d2d3e5317915a16e79acaa216584d933af71
hp <hp@192.168.2.119>
2020-05-29 20:34:34 -0400

    Second Commit --> Changed @author Daksh Arora

--- src/pickacard/Card.java
+++ src/pickacard/Card.java
@@ -7,8 +7,8 @@ package pickacard;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in Exercise 1. When you create your own branch,
  * add your name as an author.
- * @author dancye
- * @author Paul Bonenfant
+ * 
+ * @author Daksh Arora 29 May 2020
  */
 public class Card {
 
