f4cc76aec9af469615ca9d16e57f945f477a92f3
anmolsekhon590 <54895453+anmolsekhon590@users.noreply.github.com>
2020-05-30 19:32:36 -0400

    - fixed error when magicHand would not save cards in an array - added functionality for user to enter their card - program now displays all the card made randomly in magicHand card array

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,5 +1,7 @@
 package pickacard;
 
+import java.util.Scanner;
+
 /**
  * @author anmol sekhon
  * 
@@ -10,16 +12,45 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
-
+        
         for (int i = 0; i < magicHand.length; i++) {
+            
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(c.randomNumber());
+            //System.out.println(c.randomNumber());
+            
+            c.setSuit(Card.SUITS[c.randomSuit()]);
+            //System.out.println(Card.SUITS[c.randomNumber()]);
+            
+            magicHand[i] = c;
+            
         }
 
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
+        //and search magicHand here
         //Then report the result here
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter Card Value >>> ");
+        int userCardValue = scan.nextInt();
+        
+        //flushing the scanner
+        scan.nextLine();
+        
+        System.out.print("Enter Card Suit >>> ");
+        String userCardSuit = scan.nextLine();
+        
+        
+        Card userCard = new Card();
+        userCard.setSuit(userCardSuit);
+        userCard.setValue(userCardValue);
+        
+        System.out.println(userCard);
+        
+        for (int i=0; i<magicHand.length;i++) {
+            System.out.println(magicHand[i]);
+        }
+        
+        
     }
-
 }
