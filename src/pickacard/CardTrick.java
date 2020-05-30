e9068d6b14c2a7d235f2bc80d7440773548154ff
Safi K <safikh99@gmail.com>
2020-05-30 14:54:33 -0400

    Finished up the CardTrick class.

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,9 +1,13 @@
 package pickacard;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
- *
+ * 2020-05-30
+ * @author Safi Khalid
  * @author dancye
  * @author Paul Bonenfant
  */
@@ -11,17 +15,41 @@ public class CardTrick {
 
     public static void main(String[] args) {
         
+        Random randNum = new Random();
+        Scanner in = new Scanner(System.in);
         Card[] magicHand = new Card[7];
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            c.setValue(randNum.nextInt(13) + 1); 
+            c.setSuit(Card.SUITS[randNum.nextInt(4)]); 
+            magicHand[i] = c;
+            
         }
-
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        
+        Card userCard = new Card();
+        
+        System.out.print("Enter a card number: ");
+        userCard.setValue(in.nextInt());
+        System.out.print("Enter a card suit: ");
+        userCard.setSuit(in.next());
+        
+        int i = 1;
+        
+        for (Card c : magicHand) {
+            if (c == userCard){
+                System.out.printf("Card number %d in the magic hand is the"
+                        + " same as yours.\n", i);
+                break;
+                
+            }
+            else{
+                System.out.printf("Card number %d in the magic hand is NOT the"
+                        + " same as yours.\n", i);
+            }
+            i++;
+        }
     }
 
 }
