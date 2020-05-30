8537ea5384120f7bd2ebe9ffe082fa62cdc77bb4
tommychoi914 <65785126+tommychoi914@users.noreply.github.com>
2020-05-30 13:00:07 -0400

    Update CardTrick.java

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,5 +1,5 @@
 package pickacard;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -15,13 +15,33 @@ public class CardTrick {
 
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13)+1);         
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i] = c;
         }
-
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+    
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("enter card value ");
+        int value = input.nextInt();
+        
+        System.out.println("enter card suit");
+        String suit = input.next();
+        
+        int result = 0;
+        for(int i = 0; i < magicHand.length; i++){
+            if(value==magicHand[i].getValue() && suit.equalsIgnoreCase(magicHand[i].getSuit()))
+            {
+                result=1;
+            }
+        }
+        if(result==1){
+            System.out.println("Congratulations!! You are a Winner");            
+        }
+        else{
+            System.out.println("you are a loser");
+        }       
     }
 
 }
