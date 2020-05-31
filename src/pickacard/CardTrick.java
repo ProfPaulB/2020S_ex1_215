adca8f77fcd12a2c24d89de611d78540abd50e1e
Sarthak Wadhawan <66051246+sarthakwadhawan@users.noreply.github.com>
2020-05-30 23:54:29 -0400

    Update CardTrick.java

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -4,8 +4,7 @@ package pickacard;
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
- * @author dancye
- * @author Paul Bonenfant
+ * @author sarthak may30 
  */
 public class CardTrick {
 
@@ -23,6 +22,33 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the card ");     
+        int CardValue = input.nextInt();
+        input.nextLine();
+        
+        System.out.print("Enter suit ");
+        
+        String CardSuit = input.nextLine();
+
+        Card luckyCard = new Card();
+        luckyCard.setSuit(CardSuit);
+        luckyCard.setValue(CardValue);
+        
+        int matches = 0; 
+         for (int i=0; i<magicHand.length;i++) {
+                        
+            if ((magicHand[i].toString()).equals(luckyCard.toString())){
+                System.out.println("you won");
+                
+                matches++;
+            }
+            
+        }
+        if (matches == 0) {
+            System.out.println("you lost");
+        }
     }
+  }
 
 }
