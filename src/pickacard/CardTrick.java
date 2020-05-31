2aa6ca77d2fabfd6b5d8340368a98b313067c5d3
anmolsekhon590 <54895453+anmolsekhon590@users.noreply.github.com>
2020-05-30 20:25:32 -0400

    - Deleted unused code - added comments - Now user is unable to see cards in magicHand

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -11,44 +11,55 @@ public class CardTrick {
 
     public static void main(String[] args) {
         
+        // creates a new card array of length 7
         Card[] magicHand = new Card[7];
         
+        //this loop creates a new card with random values 
+        //and puts it in the array 'magicHand'
         for (int i = 0; i < magicHand.length; i++) {
             
             Card c = new Card();
-            c.setValue(c.randomNumber());
-            //System.out.println(c.randomNumber());
             
+            c.setValue(c.randomNumber());          
             c.setSuit(Card.SUITS[c.randomSuit()]);
-            //System.out.println(Card.SUITS[c.randomNumber()]);
-            
             magicHand[i] = c;
             
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        //and search magicHand here
-        //Then report the result here
+        //Scanner object 'scan' created for scanning inputs
         Scanner scan = new Scanner(System.in);
+        
+        /**
+         * an int 'match' initialized to zero 
+         * this will be used to check the number of matches
+         * found later
+         */
         int match = 0;
         
+        //prompting user to enter a value for the card
         System.out.print("Enter Card Value >>> ");
+        //scanning and storing the value in 'userCardValue'
         int userCardValue = scan.nextInt();
         
         //flushing the scanner
         scan.nextLine();
         
+        //prompting user to enter the Suit to which the card belongs
         System.out.print("Enter Card Suit >>> ");
+        
+        //scanning and storing the scanned string in 'userCardSuit'
         String userCardSuit = scan.nextLine();
         
-        
+        //creating a new userCard object for user and
+        //sets the values according to the values the user entered
         Card userCard = new Card();
         userCard.setSuit(userCardSuit);
         userCard.setValue(userCardValue);
         
+        //checking if user's card matches with any card in magicHand
+        //and counting if any match is found
         for (int i=0; i<magicHand.length;i++) {
-            System.out.println(magicHand[i]);
-            
+                        
             if ((magicHand[i].toString()).equals(userCard.toString())){
                 System.out.println("\nMatch Found!");
                 
@@ -56,6 +67,7 @@ public class CardTrick {
             }
         }
         
+        //presenting a report to user at the end
         System.out.println(userCard.report(match));
         
     }
