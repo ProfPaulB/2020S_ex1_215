ae6b30c1be6d2197f854321c95ba54bd41091119
ubaid24 <rehmauba@sheridancollege.ca>
2020-06-04 04:46:18 -0400

    Added code for CardTrick

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -6,6 +6,7 @@ package pickacard;
  *
  * @author dancye
  * @author Paul Bonenfant
+ * @author Ubaid Ur Rehman May 30,2020
  */
 public class CardTrick {
 
@@ -14,14 +15,36 @@ public class CardTrick {
         Card[] magicHand = new Card[7];
 
         for (int i = 0; i < magicHand.length; i++) {
+            
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+
+            c.setValue((int)(Math.random()*13)+1); 
+            
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            
+            magicHand[i] = c;
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        
+        Card luckyCard = new Card();
+        
+        luckyCard.setValue(4);
+        
+        luckyCard.setSuit("Clubs");
+        
+        int match = 0;
+        for(int i = 0; i < magicHand.length; i++){
+            if(luckyCard.getValue()== magicHand[i].getValue() && luckyCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit()))
+            {
+                match=1;
+            }
+        }
+        if(match==1){
+            System.out.println("You have a matching card!");            
+        }
+        else{
+            System.out.println("Sorry, your card does not match");
+        }
     }
 
 }
