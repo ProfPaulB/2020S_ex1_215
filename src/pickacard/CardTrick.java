23266e226ca92962c2b088e31e6a09a62be97d70
ubaid24 <60014146+ubaid24@users.noreply.github.com>
2020-06-04 05:06:49 -0400

    Added comments for code clarification

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,9 +1,6 @@
 package pickacard;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
- * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
- *
  * @author dancye
  * @author Paul Bonenfant
  * @author Ubaid Ur Rehman May 30,2020
@@ -13,33 +10,37 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
-
+        // loop to increase hand size to 7
         for (int i = 0; i < magicHand.length; i++) {
             
             Card c = new Card();
-
+            //generating random card number
             c.setValue((int)(Math.random()*13)+1); 
-            
+            //generating random suit
             c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
             
             magicHand[i] = c;
         }
-
-        
+        //luckyCard Obj
         Card luckyCard = new Card();
-        
+        //user picking between 1-13
         luckyCard.setValue(4);
-        
+        //User setting suit
         luckyCard.setSuit("Clubs");
         
         int match = 0;
+        
+        //loop to determine match or no match
         for(int i = 0; i < magicHand.length; i++){
-            if(luckyCard.getValue()== magicHand[i].getValue() && luckyCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit()))
+            if(luckyCard.getValue()== magicHand[i].getValue() 
+            && luckyCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit()))
             {
-                match=1;
+            match=1;
             }
         }
-        if(match==1){
+        
+        //output if card matches or not
+        if (match==1){
             System.out.println("You have a matching card!");            
         }
         else{
